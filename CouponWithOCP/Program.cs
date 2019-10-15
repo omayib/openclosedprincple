1859f9d361aa6164fcd6a1fdd9e9d2820dbbd1b2
omayib <omayib@gmail.com>
2019-10-16 02:18:32 +0700

    implementasi open-closed principle

--- CouponWithOCP/Program.cs
+++ CouponWithOCP/Program.cs
@@ -7,6 +7,21 @@ namespace CouponWithOCP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Coupon coupon1 = new CouponWithNominal(2000);
+            Console.WriteLine(coupon1.calculate(10000));
+
+
+            Coupon coupon2 = new CouponWithPercentage(25);
+            Console.WriteLine(coupon2.calculate(10000));
+
+
+            Coupon coupon3 = new CouponWithMaxNominal(90,3000);
+            Console.WriteLine(coupon3.calculate(10000));
+
+            Coupon coupon4 = new CouponWithCashback(500);
+            Console.WriteLine(coupon4.calculate(10000));
+
+
         }
     }
 }
